00818f51486aec183954fb2cd06d4a36467d91c8
Brian Donald <contact@noimbrian.com>
2022-04-01 22:21:32 -0500

    feat: environment variables

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_URL ?? process?.env?.NEXT_PUBLIC_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY ?? process?.env?.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
 export default supabase
