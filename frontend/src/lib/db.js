7a23f5950f31cb828e6f684430486298f71724cc
Brian Donald <contact@noimbrian.com>
2022-04-01 22:23:56 -0500

    fix: intensify pain

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
+import {browser} from "$app/env";
+
+
+
+
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL ?? process?.env?.NEXT_PUBLIC_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY ?? process?.env?.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  browser ? import.meta.env.VITE_SUPABASE_URL : process?.env?.NEXT_PUBLIC_SUPABASE_URL,
+  browser ? import.meta.env.VITE_SUPABASE_ANON_KEY : process?.env?.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
 export default supabase
