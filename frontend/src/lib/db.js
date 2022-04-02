6b344c1c12e7ef655fd9ae7633a6da6cc1c34579
Brian Donald <contact@noimbrian.com>
2022-04-01 22:29:04 -0500

    chore: test

--- frontend/src/lib/db.js
+++ frontend/src/lib/db.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import {browser} from "$app/env";
 
-
-
-
-
 const supabase = createClient(
   browser ? import.meta.env.VITE_SUPABASE_URL : process?.env?.NEXT_PUBLIC_SUPABASE_URL,
   browser ? import.meta.env.VITE_SUPABASE_ANON_KEY : process?.env?.NEXT_PUBLIC_SUPABASE_ANON_KEY
